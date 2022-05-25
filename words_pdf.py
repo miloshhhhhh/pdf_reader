3bdbed9c7072412b55afbc575f67a32867909065
miloshhhhhh <75224808+miloshhhhhh@users.noreply.github.com>
2022-05-25 03:10:38 +0200

    Add files via upload

---
+++ words_pdf.py
@@ -0,0 +1,18 @@
+# Language: Python
+# Count Words in pdf
+
+import PyPDF2
+import re
+
+
+def count_words_in_pdf(pdf_path):
+    pdf_file = open(pdf_path, 'rb')
+    pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+    num_pages = pdf_reader.numPages
+    count = 0
+    for i in range(num_pages):
+        page = pdf_reader.getPage(i)
+        count += len(re.findall(r'\w+', page.extract_text()))
+    return print(count)
+
+count_words_in_pdf(r'')
